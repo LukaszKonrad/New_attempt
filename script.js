a9ebc4b8751ffec03e30d40c6fcce66a94158170
Lukasz Konrad <lukasz.konrad@gmail.com>
2023-06-16 10:43:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let przycisk = document.querySelector(".przycisk");
+let zdjecie = document.querySelector(".zdjecie");
+let przycisk2 = document.querySelector(".przycisk__dwa");
+let naglowek = document.querySelector('.naglowek');
+let przycisk3 = document.querySelector(".przycisk__trzy");
+let przycisk4 = document.querySelector(".przycisk__czwarty");
+let tekst = document.querySelector(".cos");
+
+przycisk.addEventListener("click", () => {
+zdjecie.classList.toggle("hidden");
+});
+
